5d523f889c910c6a864a163f56bfc66a0d19219a
Ouardane <ouardane@crans.org>
2010-09-12 21:21:30 +0200

    Ajout des remerciements de romain

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way, it's our only promotion or advertising. -->
 	<p>
+		Merci à Spqr pour les photos publiées sur ce site.
 		<?php bloginfo('name'); ?> is proudly powered by
 		<a href="http://wordpress.org/">WordPress</a>
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
