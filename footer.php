bbb402481ce5febf11a2bd7bf46973165f2d3b3c
Ouardane <ouardane@crans.org>
2010-09-12 22:15:02 +0200

    Francisation du footer

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way, it's our only promotion or advertising. -->
 	<p>
 		Merci à Spqr pour les photos publiées sur ce site.
-		<?php bloginfo('name'); ?> is proudly powered by
+		<?php bloginfo('name'); ?> est fièrement propulsé par
 		<a href="http://wordpress.org/">WordPress</a>
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		<br /><a href="<?php bloginfo('rss2_url'); ?>">Actualités (RSS)</a>
+		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Commentaires (RSS)</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </div>
